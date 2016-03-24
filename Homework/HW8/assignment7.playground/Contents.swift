2197c8594447179d50d4ca594c96e335243da542
Arun Nagarajan <anagara@uber.com>
2016-03-24 13:55:44 -0400

    added HW8

---
+++ Homework/HW8/assignment7.playground/Contents.swift
@@ -0,0 +1,23 @@
+// Assignment 7 playground
+
+import XCPlayground
+import Foundation
+
+// To include SwiftyJSON, download SwiftyJSON.swift from the repository at 
+//   https://github.com/SwiftyJSON/SwiftyJSON/tree/master/Source
+// Then expand the Navigator with CMD+1.
+// Drag/drop the file from Finder into the "Sources" folder in the Navigator.
+// Uncomment the import statement below.
+//import SwiftyJSON
+
+// Let asynchronous code run
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+
+//TODO one: Write and call a function that make a successful network connection to google.com using core networking APIs, then print out the results.
+
+//TODO two: Write and call a function that makes a failing network connection (using core networking APIs) to http://generalassemb.ly/foobar.baz, a nonexistant page. Print out the status code and body of the response.
+
+//TODO three: Make a successful network connection to http://api.openweathermap.org/data/2.5/weather?q=New%20York,US, an API that speaks JSON using core networking APIs. Create a model class that corresponds to the JSON response object, populate it with the contents of that JSON using NSJSONSerialization, then print out the model.
+
+//TODO four: Make a successful network connection to http://api.openweathermap.org/data/2.5/weather?q=New%20York,US, an API that speaks JSON. Populate a your above-defined model with the contents of that JSON using SwiftyJSON, then print out the model.
+
